0eb467231dadec51e70abbb4614a90987a4ace82
Niraj <nirajpatidar11@gmail.com>
2019-09-14 18:44:29 +0530

    implement router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-import Hello from './components/Hello';
-import Welcome from './components/Welcome';
+import Blog from './containers/Blog/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Hello name="Niraj"/>
-        <Hello name="Shubham"/>
-        <Welcome />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Blog/>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
